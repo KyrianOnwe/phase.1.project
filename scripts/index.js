28e9f108dff8e779fd3a11183f7036302543af1b
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-23 14:05:06 -0600

    Added sidenav in HTML and js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,20 @@ const mainDiv = () => document.getElementById("main")
 
 
 /** When DOM is loaded **/
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
 
 
 /** Events **/
