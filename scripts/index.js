73e9941d9d98f0083f6a8fee041e8f5e743750e7
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-23 19:55:42 -0600

    Added links for more pages to js and tried a modal

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,33 @@
 
 /** Node Getters **/
 const mainDiv = () => document.getElementById("main") ;
-const homePg = () => document.getElementById('home-page-link')
+const homePg = () => document.getElementById('home-page-link');
     //TEST
-const testPg = () => document.getElementById("js-page-link")
+const testPg = () => document.getElementById("js-page-link");
+const mathPg = () => document.getElementById('math-page-link');
+const anatPg = () => document.getElementById('anatomy-page-link');
 
 /** Templates **/
 const homePageTemp = () => {
   return `
   <h1 class="center-align">Welcome to your study date!</h1>
+
+  <div class="container" id="modal-container">
+    <!-- Modal Trigger -->
+    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Choose a subject above.  No fooling around until you're good and smart!!</a>
+
+    <!-- Modal Structure -->
+    <div id="modal1" class="modal">
+      <div class="modal-content">
+        <h4>Modal Header</h4>
+        <p>A bunch of text</p>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+      </div>
+    </div>  
+  </div>
+
   `
 }
     //TEST
