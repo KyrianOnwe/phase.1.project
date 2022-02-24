44bb04f607c98f8ab303fd13c04ac2c8baa9cd91
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-23 18:49:10 -0600

    Added id's to links

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,39 @@
 
 
 /** Node Getters **/
-const mainDiv = () => document.getElementById("main") 
+const mainDiv = () => document.getElementById("main") ;
+const homePg = () => document.getElementById('home-page-link')
+
 
 /** Templates **/
+const homePageTemp = () => {
+  return `
+  <h1 class="center-align">Welcome to your study date!</h1>
+  `
+}
+
+
+
+/** Renderers **/
+const renderHome = () => {
+  mainDiv().innerHTML = homePageTemp();
+}
 
 
 /** When DOM is loaded **/
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  renderHome();
+  homePageJump();
+})
 
 
 /** Events **/
+function homePageJump(){
+  homePg().addEventListener('click', (e)=>{
+    renderHome();
+  })
+
+}
 
 
 
