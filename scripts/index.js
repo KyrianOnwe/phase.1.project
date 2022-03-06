655b3711eb9a24aafab49dc86464405fd91926ea
Kyrian <dr.kyrian.onwe@gmail.com>
2022-03-05 20:41:41 -0600

    Removing extra node getters

--- scripts/index.js
+++ scripts/index.js
@@ -6,167 +6,84 @@ var questNumb = 0;
 var numbIncorr = 0;
 
 
-
 /** Node Getters **/
 const mainDiv = () => document.getElementById("main") ;
 const homePg = () => document.getElementById('home-page-link');
-const mathPg = () => document.getElementById('math-page-link');
 const anatPg = () => document.getElementById('anatomy-page-link');
 
 /** Templates **/
 const homePageTemp = () => {
   return `
   <h1 class="center-align">Welcome to your study date!</h1>
-
   <div class="container" id="modal-container">
     <!-- Modal Trigger -->
-    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Make your choice above.  Let's STUDY!</a>
-
+    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Choose a subject above.  No fooling around until you're good and smart!!</a>
     <!-- Modal Structure -->
     <div id="modal1" class="modal">
       <div class="modal-content">
         <h4>Modal Header</h4>
-        <p>Make your choice above.  Let's STUDY!</p>
+        <p>Choose a subject above.  No fooling around until you're good and smart!!</p>
       </div>
       <div class="modal-footer">
         <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
       </div>
     </div>  
   </div>
-
   `
 }
-
-
+    //TEST
+const testPageTemp = () => {
+  return ``
+}
 const studyPageTemp = () => {
   return `
-      <div class="col s3 green" style="height: 600px" id="scoreboard">
-      </div>
-      <div class="col s9 blue lighten-3" style="height: 600px id="question-side">
-        <div style="height: 550px; overflow: auto; overflow-x: hidden;">
-          <div class="row" id="row2">
-          </div>
-        </div>
-        <form class="valign-wrapper">
-          <input type="text" name="" id="response-bar" class="valign-bottom white">
-          <button class="btn" id="submit">Submit</button>
+    <div class="container row">
+      <div class="container col s3">
+        <form action="/action_page.php" id="response-submit">
+          <label for="lname">The response:</label><br>
+          <input type="text" id="lname" name="lname" value=""><br><br>
+          <input type="submit" value="Submit">
         </form>
-        </div>
-       </div>
       </div>
-      `
-}
-
-function chatBox(submission){
-  let chatSham = document.createElement('div')
-  chatSham.className = "col offset-s6 offset-m6 s6 m6"
-  chatSham.innerHTML = `
-  <div class="card grey lighten-1">
-    <div class="card-content white-text">
-      <p class="blue-text text-darken-1" style="line-height: 18px;"><span class="black-text">We Say:</span> ${submission}</p>
+      <div class="col s4 teal lighten-5">
+      </div>
+      
+      <div class="col s5 teal lighten-5">
+        <div class="container" id="question-side">
+            <h1>Hi There</h1>
+        </div>
+      </div>
     </div>
-  </div>
+    
   `
-  document.querySelector("#row2").appendChild(chatSham)
-
 }
-
-
-function chatCheck(submission){
-  let jack = submission
-  let jill = testData[questNumb].answer
-  if (jack == jill){
-    return numbCorrect = parseInt(numbCorrect) + 1
-  } else {
-    return numbIncorr = parseInt(numbIncorr) + 1
-  }
-
-}
-
-function chatQuestUpd(){
-  return questNumb = parseInt(questNumb) + 1
-}
-
-
-function chatInputReset(){
-  document.getElementById('response-bar').value = null;
-}
-
-
-function chatGF(current= questNumb, array = testData){
-  
-  let gF = document.createElement('div')
-  gF.className = 'col s6 m6';
-  gF.innerHTML = `
-  <div class="card white">
-   <div class="card-content white-text">
-     <p class="grey-text text-darken-4" style="line-height: 18px;"><span class="black-text">Computer Says:</span> ${array[current].question}</p>
-   </div>
-  </div>
-  `
-  
-  document.querySelector("#row2").appendChild(gF);
-}
-
-function initialChatGF(){
-  let igf = document.createElement('div')
-  igf.className = 'col s6 m6';
-  igf.innerHTML = `
-  <div class="card white">
-  <div class="card-content white-text">
-    <p class="grey-text text-darken-4" style="line-height: 18px;"><span class="black-text">Computer Says:</span>Are you ready?</p>
-  </div>
-  </div>
-  `
-document.querySelector("#row2").appendChild(igf);
-}
-
-
-/** This is how she will ask questions **/
-
 
 
 /** Renderers **/
 const renderHome = () => {
   mainDiv().innerHTML = homePageTemp();
 }
-
+    //TEST
+const testRenderHome = () => {
+  mainDiv().innerHTML = testPageTemp();
+}
 const renderAnatPg = () => {
-  mainDiv().innerHTML = ''
-  mainDiv().className = 'row'
   mainDiv().innerHTML = studyPageTemp();
-  //showScore()
-  setTimeout(initialChatGF, 2500)
+  interject();
   handleResponse()
-  //reinforce();
-}
-
-
-
-function sayScore(){
-  let lenth = testData.length - 1
-  return `
-  <h4>${numbCorrect} of ${lenth} correct</h4>
-  `
-}
-
-function showScore(){
-  let score = () => document.querySelector('#scoreboard');
-  return score().innerHTML = sayScore()
 
 }
 
-
-
-
+function interject(){
+  document.querySelector('#question-side').innerHTML = `<h1>Yo Jo!</h1>`
+}
 /** When DOM is loaded **/
 document.addEventListener('DOMContentLoaded', () => {
-  getData();
   renderHome();
   homePageJump();
+  testPageJump();
   anatPgJump();
 })
-
 
 
 /** Events **/
@@ -176,7 +93,12 @@ function homePageJump(){
   })
 
 }
+function testPageJump(){
+  testPg().addEventListener('click', (e) => {
+    testRenderHome();
 
+  })
+}
 function anatPgJump(){
   anatPg().addEventListener('click', () => {
     renderAnatPg();
@@ -184,61 +106,10 @@ function anatPgJump(){
 }
 
 function handleResponse(){
-  let handler = document.getElementById("submit");
-  handler.addEventListener('click', (e) => {
+  let handler = document.getElementById("response-submit");
+  handler.addEventListener('submit', (e) => {
     e.preventDefault();
-    let retort = document.getElementById('response-bar').value;
-    chatBox(retort);
-    chatCheck(retort);
-    chatInputReset();
-    chatQuestUpd();
-    showScore();
-    reinforce();
-    //setTimeout(chatGF, 7500);
   })
-
-
 }
-
-
-
-/** Server calls **/
-function getData(){
-  fetch(`${baseUrl}/challenge`)
-  .then(res => res.json())
-  .then(data => writeDataTable(data))
-}
-
-function writeDataTable(pic){
-  let listCopy = [pic, ...testData];
-  return testData = listCopy[0];
-}
-
-
-
 
 /**********************************/
-
-/** Positive reinforcement **/
-function reinforce(){
-  if (parseInt(numbCorrect) == 3){
-    //setTimeout(flirt, 2000)
-    setTimeout(chatGF, 7500)
-  } else {
-    setTimeout(chatGF, 5500)
-  }
-}
-
-function flirt(){
-  let igf = document.createElement('div')
-  igf.className = 'col s6 m6';
-  igf.innerHTML = `
-  <div class="card white">
-  <div class="card-content white-text">
-    <p class="grey-text text-darken-4" style="line-height: 18px;"><span class="black-text">Computer Says:</span>Big brains are SO SEXY!</p>
-  </div>
-  </div>
-  `
-document.querySelector("#row2").appendChild(igf);
-
-}
